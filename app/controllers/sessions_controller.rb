b154759af1e925647d5f791a7bb69740c3ec3ebd
Andre Torres <andre855@gmail.com>
2016-05-05 18:43:19 -0300

    Initial commit with user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(id: params[:session][:id])
+    if !user.nil?
+      log_in user
+      flash[:success] = "Login Successful"
+      redirect_to root_path
+    else
+      flash.new[:danger] = "Login failed"
+      render :new
+    end
+  end
+  
+end
