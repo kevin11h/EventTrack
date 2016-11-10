d5838a891e342991ba0dc6dc14e06da23e2a2c01
Andre Torres <andre855@gmail.com>
2016-11-09 23:45:36 -0200

    Added dashboard page for when you login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #users
+
+  # users
   get 'profile' => 'users#profile'
   devise_for :users
-  #events
+
+  # events
   resources :events
   resources :invites, only: :index
-  post  'invites'       =>  'invites#create'
-  post  'confirmation/:id'   =>  'invites#confirm', as: "confirmation"
+  post  invites: 'invites#create'
+  post  'confirmation/:id' => 'invites#confirm', as: 'confirmation'
 
-  #static_pages and sessions
-  root 'static_pages#home'  
+  # static_pages and sessions
+  root 'static_pages#home'
+  get 'dashboard' => 'static_pages#dashboard'
 end
